80260098c6ddbe8e9ee028bbb867eafbb8c81f39
alex <alexander.kast23@gmail.com>
2024-01-02 12:40:05 +0100

    puh also wenn das funktioniert... XD

---
+++ tesseract/src/main/java/org/paperless/tesseract/TesseractApplication.java
@@ -0,0 +1,13 @@
+package org.paperless.tesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesseractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesseractApplication.class, args);
+	}
+
+}
